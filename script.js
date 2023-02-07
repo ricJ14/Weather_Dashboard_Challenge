5d13551f64121aa23e1bfccb31632c339b46f460
Ric Johnston <119433069+ricJ14@users.noreply.github.com>
2023-02-07 10:39:15 +0000

    created css file and establihed link

--- script.js
+++ script.js
@@ -5,6 +5,95 @@ let searchHistory = []
 let searchInput = $("#search-input");
 let searchForm = $("#search-form");
 let searchHistoryContainer = $("#history")
+let todayContainer = $("#today ")
+
+
+function renderSearchHistory(){
+    searchHistoryContainer.html("")
+
+    for(let i = 0; i < searchHistory.length; i++){
+        let btn = $("<button>");
+        btn.attr("type", "button") 
+        btn.addClass("history-btn btn-history")
+        btn.text(searchHistory[i])
+        btn.attr("data-search", searchHistory[i])
+        searchHistoryContainer.append(btn)
+    }
+}
+
+function appendSearchHistory(search){
+    if(searchHistory.indexOf(search) !== -1){
+        return
+    }
+    searchHistory.push(search);
+
+    localStorage.setItem("search-history", JSON.stringify(searchHistory));
+    renderSearchHistory()
+}
+
+function renderCurrentWeather(city, weatherData){
+    let date = moment().format("D/M/YYYY"); 
+    let tempC = weatherData["main"]["temp"];
+    let windKph =weatherData["wind"]["speed"];
+    let humidity = weatherData["main"]["humidity"];
+
+    let iconUrl = `http://openweathermap.org/img/w${weatherData.weather[0].icon}.png`; 
+    let iconDecription = weatherData.weather[0].description || weatherData[0].main 
+
+    let card = $("<div>")
+    let cardBody = $("<div>")
+    let weatherIcon = $("<img>")
+    let heading =  $("<h2>")
+    let tempEl = $("<p>") 
+    let windEl = $("<p>")
+
+    let humidityEl = $("<p>") 
+
+    card.attr("class", "card");
+
+    cardBody.attr("class", "card-body")
+
+    card.append(cardBody);
+
+    heading.attr("class", "h3 card-title")
+    tempEl.attr("class", "card-text")
+    windEl.attr("class", "card-text")
+    humidityEl.attr("class", "card-text");
+
+    heading.text(`${city} (${date})`)
+    weatherIcon.attr("src", iconUrl);
+    weatherIcon.attr("alt", iconDecription);
+
+    heading.append(weatherIcon);
+    tempEl.text(`Temp ${tempC} C`)
+    windEl.text(`Wind ${windKph} KPH`);
+    humidityEl.text(`Humidity ${humidity} %`)
+    cardBody.append(heading, tempEl, windEl, humidityEl);
+     
+    todayContainer.html("");
+     todayContainer.append(card);
+}
+
+
+    function fetchWeather(location){
+        let latitude = location.lat;
+        let longitude = location.lon;
+
+        let city = location.name;
+        
+        let queryWeatherURL = `${weatherAPIURL}/data/2.5/forecast?lat=${latitude}&lon=${longitude}&units=metric&appid=${weatherAPIKey}`
+
+        console.log(queryWeatherURL)
+
+        $.ajax({
+            url: queryWeatherURL,
+            method:"GET"
+        }).then(function(response){
+            renderCurrentWeather(city, response.list[0])
+            // renderForcast(data.list)
+        })
+    }
+
 
 function fetchCoord(search){
     let queryURL = `${weatherAPIURL}/geo/1.0/direct?q=${search}&limit=5&appid=${weatherAPIKey}`
@@ -15,26 +104,19 @@ function fetchCoord(search){
         if(!response[0]){
             alert("Location not found")
         } else{
-            if(searchHistory.indexOf(search) !== -1){
-                return
-            }
-            searchHistory.push(search);
-
-            localStorage.setItem("search-history", JSON.stringify(searchHistory));
-            
-            searchHistoryContainer.html("")
-
-            for(let i = 0; i < searchHistory.length; i++){
-                let btn = $("<button>");
-                btn.attr("type", "button") 
-                btn.addClass("history-btn btn-history")
-
-                btn.attr("data- search", searchHistory[i])
-                searchHistoryContainer.append(btn)
-            }
-
+            appendSearchHistory(search)
+            fetchWeather(response[0])
         }
-    })
+    });
+}
+
+function InitializeHistory(){
+    let storedHistory = localStorage.getItem("search-history");
+
+    if(storedHistory) {
+         searchHistory = JSON.parse(storedHistory);
+         renderSearchHistory()
+    }
 }
 
 function submitSearchForm(event){
@@ -42,6 +124,8 @@ function submitSearchForm(event){
     let search = (searchInput.val().trim())
 
     fetchCoord(search)
+    searchInput.val("")
 }
 
+InitializeHistory()
 searchForm.on("submit", submitSearchForm);
